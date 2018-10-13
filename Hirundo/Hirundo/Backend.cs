ee6153b456bc45e2a21c861c5115f03269fc00d9
Chris Papakrivopoulos <chrispapakr@gmail.com>
2018-10-13 18:49:05 +0300

    added a simple notification function

---
+++ Hirundo/Hirundo/Backend.cs
@@ -0,0 +1,17 @@
+﻿using Plugin.LocalNotifications;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+
+namespace Hirundo
+{
+    public class Backend
+    {
+        public void Notification(string Title, string Body, int NotifId, DateTime dt)
+        {
+            CrossLocalNotifications.Current.Show(Title, Body, NotifId, dt);
+        }
+    }
+}
