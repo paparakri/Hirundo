bf273aba6c68544fae3f9ee76e8e5aa351039797
Stavros Michalovits <stavros.michalovits@gmail.com>
2018-10-13 18:34:41 +0300

    Added a first time setup page

---
+++ Hirundo/Hirundo/Helpers/Settings.cs
@@ -0,0 +1,47 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace Hirundo.Helpers
+{
+    public static class Settings
+    {
+        private static ISettings AppSettings
+        {
+            get {
+                return CrossSettings.Current;
+            }
+        }
+
+        #region Setting Constants
+
+        private const string SettingsKey = "settings_key";
+        private static readonly string SettingsDefault = string.Empty;
+
+        #endregion
+
+        #region Name
+        private const string Username = "";
+        private static readonly string UsernameDefault = "";
+        #endregion
+        public static string GeneralSettings
+        {
+            get {
+                return AppSettings.GetValueOrDefault(SettingsKey, SettingsDefault);
+            }
+            set {
+                AppSettings.AddOrUpdateValue(SettingsKey, value);
+            }
+        }
+
+        public static string GetUsername
+        {
+            get {
+                return AppSettings.GetValueOrDefault(Username, UsernameDefault);
+            }
+            set {
+                AppSettings.AddOrUpdateValue(Username, value);
+            }
+        }
+    }
+}
+
---
+++ Hirundo/Hirundo/ViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Hirundo.Helpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Hirundo
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public Action moveto_main;
+        public ICommand SubmitName { set; get; }
+
+        public ViewModel()
+        {
+            SubmitName = new Command(SaveName);
+        }
+
+        private string username;
+        public string Username
+        {
+            get => username;
+            set {
+                username = value;
+                PropertyChanged(this, new PropertyChangedEventArgs("Username"));
+            }
+        }
+
+        public void SaveName()
+        {
+            Settings.GetUsername = Username;
+            moveto_main();
+        }
+    }
+}
