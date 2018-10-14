b0879af287d5123cd2eb9dd74bf0642771340955
Chris Papakrivopoulos <chrispapakr@gmail.com>
2018-10-14 12:32:51 +0300

    added a calendar page, without special dates

---
+++ Hirundo/Hirundo/NewHabit.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hirundo
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NewHabit : ContentPage
+	{
+		public NewHabit ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
