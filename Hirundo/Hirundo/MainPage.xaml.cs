89bd7f405642e75639e3a0bf0965cda878b9e2fb
Chris Papakrivopoulos <chrispapakr@gmail.com>
2018-11-30 20:50:21 +0200

    added getTasks

--- Hirundo/Hirundo/MainPage.xaml.cs
+++ Hirundo/Hirundo/MainPage.xaml.cs
@@ -5,17 +5,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using SQLite;
 
 namespace Hirundo
 {
 	public partial class MainPage : ContentPage
 	{
+        SQLiteConnection database;
         public MainPage()
 		{
+            database = SQLite_Android.GetConnection();
             var view_model = new ViewModel();
             BindingContext = view_model;
 
             InitializeComponent();
 		}
+
+        public List<Task> GetTasks()
+        {
+            return database.Table<Task>().ToList();
+        }
+
     }
 }
